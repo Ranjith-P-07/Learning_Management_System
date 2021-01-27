d7c41832114e1c23d5ef3d1e3cad9f37b9b84bfe
Ranjith P <Ranjithroy704@gmail.com>
2021-01-28 02:15:46 +0530

    [Ranjith]Add: Registered all models

--- Management/admin.py
+++ Management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course, Mentor, Student, Performance, Education
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Mentor)
+admin.site.register(Student)
+admin.site.register(Education)
+admin.site.register(Performance)
+
