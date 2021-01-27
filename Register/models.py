694f259b5c926ad171f94c78e3a649b7d39e329e
Ranjith P <Ranjithroy704@gmail.com>
2021-01-28 00:31:48 +0530

    [Ranjith]Add:User model is created

--- Register/models.py
+++ Register/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    mobile_number = models.CharField(max_length=13, default=None, null=True, unique=True)
+    role = models.CharField(
+        choices=(
+            ('Mentor', 'Mentor'),
+            ('Student', 'Student'),
+            ('Admin', 'Admin')
+        ), max_length=20
+    )
+
+    def get_user_name(self):
+        return self.Username
