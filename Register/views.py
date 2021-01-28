868e2cad40c408df19688f12e8eba2fdecce12c1
Ranjith P <Ranjithroy704@gmail.com>
2021-01-29 01:30:44 +0530

    [Ranjith]Add: UserRegistrationView and Userloginview is created

--- Register/views.py
+++ Register/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+class UserRegistrationView(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'response': f"New {request.data['role']} is added"}, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get('username')
+        password = serializer.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({'response': 'You are successfully logged in'}, status=status.HTTP_200_OK)
+        return Response({'response': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
