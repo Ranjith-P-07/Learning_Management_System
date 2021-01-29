fa622f133bb7a5ad5e1d0e14a5a592a1d2c07f0b
Ranjith P <Ranjithroy704@gmail.com>
2021-01-29 23:21:24 +0530

    [Ranjith]Update: Email sending feature is added

--- Register/views.py
+++ Register/views.py
@@ -4,16 +4,33 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate, login, logout
+from .permissions import Admin_validate
+from django.contrib.sites.shortcuts import get_current_site
+import sys
+
+sys.path.append('..')
+from LMS.EmailConf import Email
 
 
 class UserRegistrationView(GenericAPIView):
     serializer_class = UserRegistrationSerializer
+    permission_classes = (Admin_validate,)
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({'response': f"New {request.data['role']} is added"}, status=status.HTTP_201_CREATED)
+        data = {
+            'username': request.data['username'],
+            'password': request.data['password'],
+            'role': request.data['role'],
+            'email': request.data['email'],
+            'domain': get_current_site(request).domain
+        }
+        Email.sendEmail(Email.addingMailBody(data))
+        return Response({
+                            'response': f"Hello {request.data['username']} you were added as a {request.data['role']},Check your mail for verification"},
+                        status=status.HTTP_201_CREATED)
 
 
 class UserLoginView(GenericAPIView):
