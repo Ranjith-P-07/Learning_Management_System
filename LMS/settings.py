a786eeb3d97c3b871e6e9f86c2830876ba861141
Ranjith P <Ranjithroy704@gmail.com>
2021-01-29 23:21:48 +0530

    [Ranjith]Update: Email configuration is done

--- LMS/settings.py
+++ LMS/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
