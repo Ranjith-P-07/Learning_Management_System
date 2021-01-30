ac1b1c2487a5711a08b300acabab0ab0d2598c21
Ranjith P <Ranjithroy704@gmail.com>
2021-01-30 23:09:00 +0530

    [Ranjith]Update: logout, forgotpassword and resetpassword path  is added

--- Register/urls.py
+++ Register/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('forgotpassword/', views.ForgotPasswordView.as_view(), name='forgotpassword'),
+    path('resetpassword/<surl>/', views.ResetPassword.as_view(), name="resetpassword"),
 ]
