40afe61b2a5724328ab60c8f8ea929e0c6cc6a5e
Ranjith P <Ranjithroy704@gmail.com>
2021-01-30 23:11:03 +0530

    [Ranjith]Add: Forgot password mail function is added

--- LMS/EmailConf.py
+++ LMS/EmailConf.py
@@ -3,12 +3,20 @@ from django.core.mail import EmailMessage
 
 class Email:
     @staticmethod
-    def addingMailBody(data):
+    def addingMailBodyForRegister(data):
         domainURL = "http://" + data['domain'] + '/' + 'user/login'
         email_body = f"Hi {data['username']} You were added as a {data['role']}, and please do login by using below credentials \n" \
                      f" Username: {data['username']}, Password: {data['password']} \n" \
                      f"Link: {domainURL}"
         email_data = {'email_body': email_body, 'email_subject': "Login to your account", 'to_email': data['email']}
+        return email_data
+
+    @staticmethod
+    def addingMailBodyForForgot(data):
+        domainURL = "http://" + data['domain'] + '/' + 'user/resetpassword/' + data['surl'] + '/'
+        email_body = f"Hi {data['username'].username} Change your password by using this link \n" \
+                     f"Link: {domainURL}"
+        email_data = {'email_body': email_body, 'email_subject': "Change your Password", 'to_email': data['username'].email}
         return email_data
 
     @staticmethod
