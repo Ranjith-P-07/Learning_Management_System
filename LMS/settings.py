4a6421f9118befab075746c12a15d136e34fb7aa
Ranjith P <Ranjithroy704@gmail.com>
2021-02-01 08:30:28 +0530

    [Ranjith]Add: Postgresql database is configured

--- LMS/settings.py
+++ LMS/settings.py
@@ -89,9 +89,14 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lms',
+        'USER': 'ranjit',
+        'PASSWORD': 'testing1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 AUTH_USER_MODEL = 'Register.User'
