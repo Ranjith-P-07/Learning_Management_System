a48cb69d7fa70b5c1f7fb4a875b4c815a2fc1a12
Ranjith P <Ranjithroy704@gmail.com>
2021-02-03 23:12:50 +0530

    [Ranjith]Add: Mentor permission class and Student permission class is added

--- Register/permissions.py
+++ Register/permissions.py
@@ -7,3 +7,19 @@ class Admin_validate(permissions.BasePermission):
         if request.user.role == 'Admin':
             return True
         return False
+
+
+class Mentor_validate(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.role == 'Mentor':
+            return True
+        return False
+
+
+class Student_validate(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.role == 'Student':
+            return True
+        return False
