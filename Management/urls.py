e25798116e8f5b4ff03de61de4f93c76a9ac9c34
Ranjith P <Ranjithroy704@gmail.com>
2021-02-05 01:46:10 +0530

    [Ranjith]Add: student-personal path, student-education path, student-course-mentor path is added

--- Management/urls.py
+++ Management/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('course-update/<int:id>/', views.CourseUpdateAPIView.as_view(), name='course_update'),
     path('mentor-list/', views.MentorAPIView.as_view(), name='mentor_List'),
     path('mentor-update/<int:id>/', views.MentorUpdateAPIView.as_view(), name='mentor_update'),
+    path('student-personal/<int:id>/', views.StudentPersonalDetailAPIView.as_view(), name='student_update'),
+    path('student-education/<int:id>/', views.StudentEducationAPIView.as_view(), name='student-education'),
+    path('student-course-mentor/', views.StudentCourseMentorMapAPIView.as_view(), name='student-course-mentor'),
+    path('student-course-mentor-update/<int:id>/', views.StudentCourseMentorUpdateAPIView.as_view(), name='student-course-mentor-update'),
 ]
