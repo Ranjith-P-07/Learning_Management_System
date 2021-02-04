ea0fd5ffbe11a701776c740992268da73c570765
Ranjith P <Ranjithroy704@gmail.com>
2021-02-05 01:37:46 +0530

    [Ranjith]Add: IsMentorAdmin permission class is added

--- Register/permissions.py
+++ Register/permissions.py
@@ -2,24 +2,21 @@ from rest_framework import permissions
 
 
 class Admin_validate(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        if request.user.role == 'Admin':
-            return True
-        return False
+        return request.user.role == 'Admin'
 
 
 class Mentor_validate(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        if request.user.role == 'Mentor':
-            return True
-        return False
+        return request.user.role == 'Mentor'
+
+
+class IsMentorAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'Mentor' or request.user.role == 'Admin'
 
 
 class Student_validate(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        if request.user.role == 'Student':
-            return True
-        return False
+        return request.user.role == 'Student'
+
