f233869e4fc632f347dd2b9003d050522011c8d4
Ranjith P <Ranjithroy704@gmail.com>
2021-02-05 01:39:36 +0530

    [Ranjith]Add: StudentSerializer, studentEducationserializer, studentcourseMentorSerializer, StudentCourseReadSerializer is added

--- Management/serializers.py
+++ Management/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Course, Mentor, Student
+from .models import Course, Mentor, Student, Education, StudentCourseMentor
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -14,7 +14,7 @@ class MentorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Mentor
-        fields = ['mentor', 'course']
+        fields = '__all__'
 
 
 class MentorUpdateSerializer(serializers.ModelSerializer):
@@ -25,3 +25,47 @@ class MentorUpdateSerializer(serializers.ModelSerializer):
         fields = ['mentor', 'course']
 
 
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class StudentEducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = '__all__'
+
+
+class StudentCourseMentorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentCourseMentor
+        fields = ['student', 'course', 'mentor', 'create_by']
+        extra_kwargs = {'course': {'required': True}, 'mentor': {'required': True}, 'create_by': {'read_only': True}}
+
+    def validate(self, data):
+        data['create_by'] = self.context['user']
+        return data
+
+
+class StudentCourseMentorReadSerializer(serializers.ModelSerializer):
+    student = serializers.StringRelatedField(read_only=True)
+    mentor = serializers.StringRelatedField(read_only=True)
+    course = serializers.StringRelatedField(read_only=True)
+    create_by = serializers.StringRelatedField(read_only=True)
+    updated_by = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = StudentCourseMentor
+        fields = "__all__"
+
+
+class StudentCourseMentorUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentCourseMentor
+        fields = ['course', 'mentor', 'updated_by']
+        extra_kwargs = {'course': {'required': True}, 'mentor': {'required': True}, 'updated_by': {'read_only': True}}
+
+    def validate(self, data):
+        data['updated_by'] = self.context['user']
+        return data
