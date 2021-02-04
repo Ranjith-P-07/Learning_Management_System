a8fd72da521115fe472765e2832226486f2c0319
Ranjith P <Ranjithroy704@gmail.com>
2021-02-05 01:36:26 +0530

    [Ranjith]Updated: current_score and updated_at field is added to Performance model

--- Management/models.py
+++ Management/models.py
@@ -25,6 +25,7 @@ class Mentor(models.Model):
 
 class Student(models.Model):
     student = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='defaultimage.png', upload_to='profile_pics')
     alternate_number = models.CharField(max_length=13, default=None, null=True, blank=True)
     Relationship_with_alternate_contact_person = models.CharField(max_length=50, default=None, null=True, blank=True)
     current_location = models.CharField(max_length=50, default=None, null=True, blank=True)
@@ -38,29 +39,28 @@ class Student(models.Model):
 
 class Education(models.Model):
     student = models.OneToOneField(Student, on_delete=models.CASCADE)
-    institute = models.CharField(max_length=50, default=None, null=True, blank=True)
-    degree = models.CharField(max_length=50, default=None, null=True, blank=True)
-    stream = models.CharField(max_length=50, default=None, null=True, blank=True)
+    institute = models.CharField(max_length=50, default=None, blank=True)
+    degree = models.CharField(max_length=50, default=None, blank=True)
+    stream = models.CharField(max_length=50, default=None, blank=True)
     percentage = models.FloatField(default=None, null=True, blank=True)
     from_date = models.DateField(default=None, blank=True, null=True)
     till = models.DateField(default=None, blank=True, null=True)
 
-    def __str__(self):
-        return self.student.student.get_user_name()
-
 
 class StudentCourseMentor(models.Model):
     student = models.OneToOneField(Student, on_delete=models.CASCADE)
-    course = models.OneToOneField(Course, on_delete=models.SET_NULL, null=True)
-    mentor = models.OneToOneField(Mentor, on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    create_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_by')
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='updated_by')
 
 
 class Performance(models.Model):
     student = models.OneToOneField(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     mentor = models.ForeignKey(Mentor, on_delete=models.SET_NULL, null=True)
-    assesment_day = models.CharField(max_length=10, default=None, blank=True, null=True)
-    current_score = models.FloatField()
+    current_score = models.FloatField(blank=True, null=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.student.get_user_name()
